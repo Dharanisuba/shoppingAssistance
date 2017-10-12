12d2989f2cb131624706955181f5b2377c4ec275
Dharanisuba <32328427+Dharanisuba@users.noreply.github.com>
2017-10-12 13:00:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,27 @@ function scanPrinter(appAi) {
 function buyPrinter (appAi) {
   //appAi.tell('Sure, I can help you with that.');
   appAi.ask('Sure, I can help you with that. \nDo you want this for home use or office use?');
+var messageData = {
+    recipient: {
+      id: senderID
+    },
+    "message":{
+      "text": "Sure. I can help you with that. \nHow do you plan on using it?",
+      "quick_replies":[
+        {
+          "content_type":"text",
+          "title":"For Personal use",
+          "payload":"PRINTER_USE_TYPE_PERSONAL"
+        },
+        {
+          "content_type":"text",
+          "title":"For Professional use",
+          "payload":"PRINTER_USE_TYPE_PROFESSIONAL"
+        }]
+    }
+  };
+
+  callSendAPI(messageData);	
 
 }
 function printerUse(appAi){
@@ -73,5 +94,32 @@ function printerUse(appAi){
 function wifiPrinter(appAi){
   appAi.ask('Do you want  the printer to print over the WiFi?');  
 }
+
+
+function callSendAPI(messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: messageData
+
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+
+        if (messageId) {
+          console.log("Successfully sent message with id %s to recipient %s",
+            messageId, recipientId);
+        } else {
+        console.log("Successfully called Send API for recipient %s",
+          recipientId);
+        }
+      } else {
+        console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+      }
+    });
+  }
+
 
 module.exports = app;
