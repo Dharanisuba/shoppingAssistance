07df40a975205cd5170ed8393b77e5a9db51dabd
Dharanisuba <32328427+Dharanisuba@users.noreply.github.com>
2017-10-12 12:53:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,12 +33,7 @@ app.post('/helloHttp', function(request, response) {
   console.log("\nReq: \n", req);
 
   data = req.originalRequest.data;
-  console.log("\ndata: \n", data);
-  var result = req.result;
-  //console.log("result", result);
-  for(var i=0; i<result.contexts.length; i++) {
-    console.log("Context: ", result.contexts[i]);
-  }
+  
   senderID = data.sender.id;
   console.log("SenderID: ", senderID);
 	
