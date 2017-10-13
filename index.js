703a3fbc3df5ca700eb5a8e9a0b3361163e8bca5
Dharanisuba <32328427+Dharanisuba@users.noreply.github.com>
2017-10-13 15:29:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.static('public'));
 app.post('/helloHttp', function(request, response) {
   console.log("Inside /helloHttp");
   var req = request.body.id;
-  console.log("\nReq: \n", req.id);
+  console.log("\nReq: \n", req);
   senderID=req; 
   console.log("\ndata: \n", response.body.id);
 	
