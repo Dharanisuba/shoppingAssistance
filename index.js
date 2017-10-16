fb3f9642b355a7dd00c21e2b343e618cd85655fc
Dharanisuba <32328427+Dharanisuba@users.noreply.github.com>
2017-10-16 13:30:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ var messageData = {
         }]
     }
   };
+	callSendAPI(messageData)
 }
 function printerUse(appAi){
    appAi.ask('Cool. Would you print a lot every day? Like more than 50 pages per week i.e moderate use or regular use?');
@@ -105,6 +106,33 @@ function printerUse(appAi){
 function wifiPrinter(appAi){
   appAi.ask('Do you want  the printer to print over the WiFi?');  
 }
+
+
+function callSendAPI(messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: messageData
+
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+
+        if (messageId) {
+          console.log("Successfully sent message with id %s to recipient %s",
+            messageId, recipientId);
+        } else {
+        console.log("Successfully called Send API for recipient %s",
+          recipientId);
+        }
+      } else {
+        console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+      }
+    });
+  }
+
 
 
  function setupGetStartedButton(res){
