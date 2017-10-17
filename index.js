d25db6ebdca9a56ace2321c5b51222572e04f0bc
Dharanisuba <32328427+Dharanisuba@users.noreply.github.com>
2017-10-17 13:16:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function callSendAPI(messageData) {
       if (!error && response.statusCode == 200) {
         var recipientId = body.recipient_id;
         var messageId = body.message_id;
-
+        console.log("message"+messageId);
         if (messageId) {
           console.log("Successfully sent message with id %s to recipient %s",
             messageId, recipientId);
